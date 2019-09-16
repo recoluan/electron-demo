e1c59f632e1f4297632ac24615a64e8063949063
reco_luan <recoluan@qq.com>
2019-09-16 13:20:37 +0800

    test: autoUpdate1.0.0

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// 注意这个autoUpdater不是electron中的autoUpdater
+import { autoUpdater } from "electron-updater"
 // 引入electron并创建一个Browserwindow
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -48,3 +50,48 @@ app.on('activate', function () {
 })
 
 // 你可以在这个脚本中续写或者使用require引入独立的js文件.   
+
+// 检测更新，在你想要检查更新的时候执行，renderer事件触发后的操作自行编写
+function updateHandle(){
+    let message={
+      error:'检查更新出错',
+      checking:'正在检查更新……',
+      updateAva:'检测到新版本，正在下载……',
+      updateNotAva:'现在使用的就是最新版本，不用更新',
+    };
+    const os = require('os');
+    autoUpdater.setFeedURL('放最新版本文件的文件夹的服务器地址');
+    autoUpdater.on('error', function(error){
+      sendUpdateMessage(message.error)
+    });
+    autoUpdater.on('checking-for-update', function() {
+      sendUpdateMessage(message.checking)
+    });
+    autoUpdater.on('update-available', function(info) {
+        sendUpdateMessage(message.updateAva)
+    });
+    autoUpdater.on('update-not-available', function(info) {
+        sendUpdateMessage(message.updateNotAva)
+    });
+    
+    // 更新下载进度事件
+    autoUpdater.on('download-progress', function(progressObj) {
+        mainWindow.webContents.send('downloadProgress', progressObj)
+    })
+    autoUpdater.on('update-downloaded',  function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+        ipcMain.on('isUpdateNow', (e, arg) => {
+            //some code here to handle event
+            autoUpdater.quitAndInstall();
+        })
+        mainWindow.webContents.send('isUpdateNow')
+    });
+    
+    //执行自动更新检查
+    autoUpdater.checkForUpdates();
+}
+
+// 通过main进程发送事件给renderer进程，提示更新信息
+// mainWindow = new BrowserWindow()
+function sendUpdateMessage(text){
+    mainWindow.webContents.send('message', text)
+}
